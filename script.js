6763e6d6d20c17a5e8bc74b0c9d24ca427852057
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-19 06:11:33 -0400

    Add do now

---
+++ script.js
@@ -0,0 +1,18 @@
+let header = document.querySelector("h1");
+let button = document.querySelector("button");
+let img = document.querySelector("img");
+
+// 1. Press the button on the webpage to change it up! Take notice of what happens.
+// - What do you think innerHTML does?
+// - What do you think .src does?
+
+
+// 2. Try the following:
+// - Make it so that the header says your name when the button is clicked.
+// - Change the src of the image to something random.
+button.addEventListener("click", function() {
+    header.innerHTML = "🤯 wow cool neeto";
+    img.src = "https://media4.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif";
+});
+
+
