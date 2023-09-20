162e3f7e771d272f762fc55d88655bc5dc2047d1
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-20 14:53:04 -0400

    Add code along and solo activity

---
+++ domManipulation.js
@@ -0,0 +1,40 @@
+// html button elements
+let contentButton = document.querySelector('.content-button');
+let image1Button = document.querySelector('.image-1-button');
+let image2Button = document.querySelector('.image-2-button');
+
+// html content elements
+let para = document.querySelector('.paragraph');
+let image = document.querySelector('.image');
+
+// CODE ALONG
+// 1. An event listener in provided for the `contentButton`
+// 2. When clicked, it should change the content of:
+//   - The `name` header.
+
+// CODE SOLO
+//   - The `hobby` paragraph.
+//   - The `location` paragraph.
+
+contentButton.addEventListener('click', function() {
+
+
+
+
+
+
+});
+
+// 3. Add an event listener to the `image1Button`
+// 4. When clicked, it should change the source of `image` to another image URL.
+
+
+
+
+
+// 5. Add an event listener to the `image2Button`
+// 6. When clicked, it should change the source of `image` to another image URL.
+
+
+// BONUS
+// 7. Try using different event types for your buttons.
